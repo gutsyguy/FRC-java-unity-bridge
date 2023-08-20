193d6a82f9a33ba603e98d68e29761f9c7844285
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2023-08-20 15:16:45 -0700

    Init

---
+++ src/main/java/com/example/javarobotcontroller/JavaRobotControllerApplication.java
@@ -0,0 +1,48 @@
+package com.example.javarobotcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+@RequestMapping("/api/robot")
+public class JavaRobotControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRobotControllerApplication.class, args);
+	}
+
+	@GetMapping("/moveForward")
+	public String moveRobotForward() {
+		// Call your robot move function here.
+		return "Robot moved forward!";
+	}
+
+	@GetMapping("/turnLeft")
+	public String turnRobotLeft() {
+		// Call your robot move function here.
+		return "Robot turned left!";
+	}
+
+	@GetMapping("/turnRight")
+	public String turnRobotRight() {
+		// Call your robot move function here.
+		return "Robot turned right!";
+	}
+
+	@GetMapping("/moveBackwards")
+	public String moveRobotBackwards() {
+		// Call your robot move function here.
+		return "Robot moved Backwards!";
+	}
+
+	@GetMapping("/stop")
+	public String stopRobot() {
+		// Call your robot stop function here.
+		return "Robot stopped!";
+	}
+
+}
---
+++ src/test/java/com/example/javarobotcontroller/JavaRobotControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javarobotcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRobotControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
